df5f8361a7e9ad1e85b0a599a232e2b6a13042f4
Саша Лебедева <sashyla1809@gmail.com>
2023-01-24 16:36:05 +0300

    Начало

--- MVVM/ViewModel.cs
+++ MVVM/ViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MVVM
 {
@@ -19,7 +20,7 @@ namespace MVVM
             }
         }
 
-        int cbIndex;
+        int cbIndex = -1;
         public int IndexSelected
         {
             set
@@ -27,6 +28,38 @@ namespace MVVM
                 cbIndex = value;
                 PropertyChanged(this, new PropertyChangedEventArgs("CBIndex"));
             }
+        }
+
+        public string CBIndex // свойство для отображения фамилии в Combobox
+        {
+            get
+            {
+                if (cbIndex == -1)
+                {
+                    return "";
+                }
+                else
+                {
+                    return Model.dataListOperation[cbIndex];
+                }
+
+            }
+        }
+
+        // класс, который реализазует интерфейс ICommand
+        public RoutedCommand Command { get; set; } = new RoutedCommand();
+
+        // обработчик события для Command (увеличение значения числа на 1)
+        public void Command_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //Model.count++;
+            PropertyChanged(this, new PropertyChangedEventArgs("CountChange"));
+        }
+        public CommandBinding bind;
+        public ViewModel()
+        {
+            bind = new CommandBinding(Command);
+            //bind.Execute += Command_Executed;
         }
     }
 }
